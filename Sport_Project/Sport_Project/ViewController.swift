7184b1a95b49ed9b849c7d725da06d53296dbfa7
Thanh Tùng <lttungcntt@gmail.com>
2017-01-13 20:28:17 +0700

    Add project ios sport

---
+++ Sport_Project/Sport_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sport_Project
+//
+//  Created by Thanh Tùng on 1/13/17.
+//  Copyright © 2017 AllAboutSport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
