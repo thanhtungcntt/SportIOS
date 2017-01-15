3ae57d795afe2f2ec0330fdf64747ad1ca4c85b3
Thanh Tùng <lttungcntt@gmail.com>
2017-01-15 13:13:57 +0700

    add file identifier

---
+++ Sport_Project/Sport_Project/Identifiers/Identifier.swift
@@ -0,0 +1,14 @@
+//
+//  Identifier.swift
+//  Sport_Project
+//
+//  Created by Thanh Tùng on 1/13/17.
+//  Copyright © 2017 AllAboutSport. All rights reserved.
+//
+
+import Foundation
+class identifier {
+   
+   static var HOME_CELL = ""
+    
+}
